7be65f448a73b4e12d42cc44aba25c5a68480600
TheBestMario <mariogeorgiev02@gmail.com>
2024-11-27 16:33:41 +0000

    Took me longer than it should have.

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //making test profiles saved.
+        Profile.getProfileArray()[0] = new Profile("Admin", "admin123");
+        Profile.getProfileArray()[1] = new Profile("MarioG", "QwErTy123");
+        Scanner scanner = new Scanner(System.in);
+        String username;
+        String password;
+        Profile currentLogin = null;
+
+        while (currentLogin == null) {
+            System.out.println("""
+                    Login Menu
+                    Enter Username:
+                    """);
+
+            username = scanner.nextLine();
+            if (username.isBlank()) {
+                System.out.println("Username cannot be empty or only have spaces");
+                continue;
+            }
+
+            System.out.println("Enter Password:");
+            password = scanner.nextLine();
+            if (password.isBlank()) {
+                System.out.println("password cannot be empty or only have spaces");
+                continue;
+            }
+
+            Profile attemptedLogin = new Profile(username, password);
+            boolean profileExists = Profile.checkUsername(username);
+            Profile profile = Profile.getProfile(username);
+            //check if profile exists
+            if (!profileExists) {
+                System.out.println("""
+                        Creating new profile...
+                        """);
+                currentLogin = new Profile(username, password);
+                currentLogin.setLoggedIn(true);
+
+            }else if (profileExists && profile.checkPassword(password)) {
+                currentLogin = Profile.getProfile(attemptedLogin.getUsername());
+                currentLogin.setLoggedIn(true);
+           }else
+               System.out.println("""
+                        The username and password combination is incorrect.
+                        Please use correct details or use a new unique username.
+                        """);
+
+
+        }
+        currentLogin.welcomeMessage();
+
+    }
+}
---
+++ src/Profile.java
@@ -0,0 +1,57 @@
+public class Profile {
+    private String username;
+    private String password;
+
+    private boolean loggedIn = false;
+    private static Profile[] profiles = new Profile[10];
+    public Profile(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+    public Profile(String username){
+        this(username, null);
+    }
+
+    public void welcomeMessage(){
+        System.out.println("Successful login, "+ username);
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    //for the sake of testing, we use this method to set some profiles
+    public static Profile[] getProfileArray() {
+        return profiles;
+    }
+    public boolean checkPassword(String password){
+        if (this.password == null) {
+            return false;
+        }
+        return this.password.equals(password);
+    }
+    public static boolean checkUsername(String username){
+        for (Profile profile : profiles) {
+            if (profile != null && profile.getUsername().equalsIgnoreCase(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Profile getProfile(String username){
+        for (Profile profile : profiles) {
+            if (profile != null && profile.getUsername().equalsIgnoreCase(username)) {
+                return profile;
+            }
+        }
+        return null;
+    }
+
+}
