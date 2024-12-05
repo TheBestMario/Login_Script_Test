55c65e64bc7bfe4e1c952163583decb1289e773a
TheBestMario <mariogeorgiev02@gmail.com>
2024-12-05 14:59:52 +0000

    updated test cases

--- Testing/ProfileTest.java
+++ Testing/ProfileTest.java
@@ -1,43 +1,45 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
     Profile testProfile = new Profile("test", "test123");
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setLoggedIn() {
         testProfile.setLoggedIn(true);
         assertTrue(testProfile.isLoggedIn());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isLoggedIn() {
         testProfile.setLoggedIn(true);
         assertTrue(testProfile.isLoggedIn());
     }
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getUsername() {
         assertEquals("test", testProfile.getUsername());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getProfileArray() {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void checkPassword() {
         assertTrue(testProfile.checkPassword("test123"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void checkUsername() {
         Profile.addProfile(testProfile);
         assertTrue(Profile.checkUsername("test"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void addProfile() {
         Profile.addProfile(testProfile);
         assertEquals(testProfile, Profile.getProfile("test"));
